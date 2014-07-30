9fa3409fcc5d2a3f3166eefb06882cb838a7a410
Quinn Slack <qslack@qslack.com>
2014-07-29 21:13:33 -0700

    add doc comments

--- animal.js
+++ animal.js
@@ -1,15 +1,19 @@
+// An Animal is a friendly creature that can say hello and make a noise.
 function Animal(name) {
   this.name = name;
 }
 
+// sayHello greets the caller.
 Animal.prototype.sayHello = function() {
   return 'Hello from ' + this.name;
 };
 
+// makeNoise emits the animal's characteristic noise.
 Animal.prototype.makeNoise = function() {
   return this.noise || '<chirp>';
 };
 
+// A Dog is an animal.
 function Dog(name, breed) {
   this.name = name;
   this.breed = breed;
@@ -18,10 +22,12 @@ function Dog(name, breed) {
 
 Dog.prototype = new Animal();
 
+// sayExtendedHello gives a doggy greeting.
 Dog.prototype.sayExtendedHello = function() {
   return this.sayHello() + ', ' + this.breed;
 };
 
+// bark barks.
 Dog.prototype.bark = function() {
   return this.noise;
 };
